344b1b65832c5028fa63dd25579f44b8a9c40304
Ali Arkate <drive2winjoy@gmail.com>
2025-06-11 18:05:34 +0400

    spritestuff

--- src/Back_Pic_Coords_Table.c
+++ src/Back_Pic_Coords_Table.c
@@ -4371,7 +4371,7 @@ const struct MonCoords gMonBackPicCoords[NUM_SPECIES] =
 	[SPECIES_PIDGEOT_MEGA] =
 	{
 		.size = 0x0,
-		.y_offset = 0x3,
+		.y_offset = 0x7,
 	},
 	[SPECIES_ALAKAZAM_MEGA] =
 	{
